1300763da57b1227b89bdc9ca56d7b77dde0b778
adi <zadi618@gmail.com>
2022-04-10 16:24:23 +0200

    ngrok update

--- src/api/apiReq.js
+++ src/api/apiReq.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://048d-109-163-161-163.ngrok.io",
+  baseURL: " https://da0b-109-163-161-163.ngrok.io",
   headers: {
     Authorization: `Bearer ${localStorage.getItem("token")}`,
   },
